b602b2a26a13a3cfea1a24c6cef7f5d023c18db5
Barsa <barsbold0102@gmail.com>
2024-04-01 14:43:07 +0800

    header heseg buren hiij duusgalaa lastfinish

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ require("postcss-import"),
 require("postcss-mixins"),
 require("postcss-simple-vars"),
 require("postcss-nested"), 
-require("autoprefixer") 
+require("autoprefixer"), 
+require("postcss-hexrgba"),
 ];
 module.exports = {
     entry: "./app/assets/script/App.js",
