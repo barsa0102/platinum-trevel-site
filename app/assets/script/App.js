9c21e49b3268202383a8cd7eab92349f0ae14ee3
barsa0102 <barsbold0102@gmail.com>
2024-04-05 14:46:35 +0800

    reveal feature buren hiij duusgav

--- app/assets/script/App.js
+++ app/assets/script/App.js
@@ -1,8 +1,12 @@
-import "../styles/styles.css"
+import "../styles/styles.css";
 import MobileMenu from "./moduls/MobileMenu";
+import Revealonscroll from "./moduls/Revealonscroll";
+
+new Revealonscroll(document.querySelectorAll(".feature-item"), 75);
+new Revealonscroll(document.querySelectorAll(".testimonials"), 50);
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
