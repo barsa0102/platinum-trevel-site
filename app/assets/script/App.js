fc9a166d87874ec42837a17238398b7eab24ec29
barsa0102 <barsbold0102@gmail.com>
2024-04-06 03:48:07 +0800

    sticky headeriig buren hiij duusgalaa

--- app/assets/script/App.js
+++ app/assets/script/App.js
@@ -1,7 +1,9 @@
 import "../styles/styles.css";
 import MobileMenu from "./moduls/MobileMenu";
 import Revealonscroll from "./moduls/Revealonscroll";
+import StickyHeaderfuck from "./moduls/StickyHeaderfuck";
 
+let stickyHeaderfuck = new StickyHeaderfuck();
 new Revealonscroll(document.querySelectorAll(".feature-item"), 75);
 new Revealonscroll(document.querySelectorAll(".testimonials"), 50);
 
