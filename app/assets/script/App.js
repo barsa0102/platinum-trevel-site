7108599897bafea6f5c0f35153a2235f75eac487
barsa0102 <barsbold0102@gmail.com>
2024-04-06 06:00:30 +0800

    Modul overly buren hiij duuslaa

--- app/assets/script/App.js
+++ app/assets/script/App.js
@@ -3,11 +3,27 @@ import MobileMenu from "./moduls/MobileMenu";
 import Revealonscroll from "./moduls/Revealonscroll";
 import StickyHeaderfuck from "./moduls/StickyHeaderfuck";
 
-let stickyHeaderfuck = new StickyHeaderfuck();
+new StickyHeaderfuck();
 new Revealonscroll(document.querySelectorAll(".feature-item"), 75);
 new Revealonscroll(document.querySelectorAll(".testimonials"), 50);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/*webpackChunkname: "modal"*/ "./moduls/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch(() => console.log("Filiig duudhad aldaa garlaa"));
+    } else {
+      modal.openModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
